277f57287d6ce5ad02c9ac1bf0e96d54f7d4a731
ansb1313 <45625135+ansb1313@users.noreply.github.com>
2022-02-25 22:23:13 +0900

    b

--- src/view/routes/Routes.js
+++ src/view/routes/Routes.js
@@ -1,13 +1,13 @@
 import React from "react";
-import {HashRouter as Router, Route, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import NotFound from "../pages/Results/NotFound";
 import Main from "../pages/Main/Main";
 
 const Routes = () => {
     return (
-        <Router>
-            <Route exact path={process.env.PUBLIC_URL + "/"} component={Main} />
-        </Router>
+        <Switch>
+            <Route exact path="/" component={Main} />
+        </Switch>
     );
 };
 
