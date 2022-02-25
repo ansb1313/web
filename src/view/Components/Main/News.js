f1d1c324fd0fae910535ef6638e003151d85ac8d
ansb1313 <45625135+ansb1313@users.noreply.github.com>
2022-02-25 21:45:09 +0900

    last

--- src/view/Components/Main/News.js
+++ src/view/Components/Main/News.js
@@ -170,6 +170,7 @@ const NewItemWrap = styled.div`
   width: 1000px;
   margin-top: 76px;
 `
+
 const NewsItem = styled.div`
   border-radius: 10px;
   box-shadow: rgba(17, 17, 26, 0.1) 0px 4px 16px, rgba(17, 17, 26, 0.1) 0px 8px 24px, rgba(17, 17, 26, 0.1) 0px 16px 56px;
